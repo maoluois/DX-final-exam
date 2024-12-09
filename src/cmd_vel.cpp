b9f68e5e83ac58a7bc72e6cf383ae4a575784f25
luomao <3214283533@qq.com>
2024-12-09 14:45:53 +0800

    Finish Task5.

--- src/cmd_vel.cpp
+++ src/cmd_vel.cpp
@@ -8,14 +8,14 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "cmd_vel");
   ros::NodeHandle nh;
 
-  ros::Publisher cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 10);
-  ros::Rate rate(10);
+  ros::Publisher cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+  ros::Rate rate(100);
 
   while (ros::ok())
   {
     geometry_msgs::Twist cmd_vel_msg;
-    cmd_vel_msg.linear.x = 1.0;
-    cmd_vel_msg.angular.z = 0.5;
+    cmd_vel_msg.linear.x = 0.0;
+    cmd_vel_msg.angular.z = 0.0;
 
     cmd_vel_pub.publish(cmd_vel_msg);
     rate.sleep();
